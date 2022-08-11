e04cf7e895d455bb3715ba5445beb8013d59f140
Yunusali <37039084+Khakimjanovich@users.noreply.github.com>
2022-08-11 16:21:41 +0500

    implemented authentication documentation

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -3,17 +3,36 @@ import { AuthService } from "./auth.service";
 import { RegisterDto } from "./dto/register.dto";
 import { LocalAuthGuard } from "./guards/local-auth.guard";
 import { JwtAuthGuard } from "./guards/jwt-auth.guard";
+import { ApiOkResponse, ApiOperation, ApiTags } from "@nestjs/swagger";
 
+@ApiTags("Authentication")
 @Controller("auth")
 export class AuthController {
   constructor(private readonly authService: AuthService) {
   }
 
+
+  @ApiOperation({ summary: "Registering a new user!" })
+  @ApiOkResponse({
+    schema: {
+      example: {
+        access_token: "sometokenstring"
+      }
+    }
+  })
   @Post("/register")
   register(@Body() registerDto: RegisterDto) {
     return this.authService.register(registerDto);
   }
 
+  @ApiOperation({ summary: "Logging in an existing user!" })
+  @ApiOkResponse({
+    schema: {
+      example: {
+        access_token: "sometokenstring"
+      }
+    }
+  })
   @HttpCode(200)
   @UseGuards(LocalAuthGuard)
   @Post("/login")
